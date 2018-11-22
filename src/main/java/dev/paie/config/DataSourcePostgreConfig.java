d1d6919e6e7b81dd88d330c4b910b50a9133d04e
formation <formation@DESKTOP-M83VDPI.DIGINAMIC.LOCAL>
2018-11-22 16:12:26 +0100

    message

--- src/main/java/dev/paie/config/DataSourcePostgreConfig.java
+++ src/main/java/dev/paie/config/DataSourcePostgreConfig.java
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-@Configuration
 public class DataSourcePostgreConfig {
 
-	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.postgresql.Driver");
